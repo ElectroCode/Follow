a5ead7cebacfd938efb0e35358b8e5021a137271
Ken Spencer <ken@electrocode.net>
2017-11-19 07:40:03 -0500

    Initial Commit 2

--- setup.py
+++ setup.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import follow as module
 
-name = module.__name__
-version = module.__version__
-url_base = "http://www.ultimate.com/phil/python/"
-url = url_base + '#' + name
-download_url = "%sdownload/%s-%s.tar.gz" % (url_base, name, version)
-# extract description and long_description from module __doc__ string
-lines = module.__doc__.split("\n")
-while len(lines) > 0 and lines[0] == '':
-    lines.pop(0)
-empty = lines.index('')
-descr = '\n'.join(lines[:empty])
-long_descr = '\n'.join(lines[empty+1:])
-
-setup(name=name,
-      version=version,
-      description=descr,
+setup(name="",
+      version="",
+      description="",
       # ReStructuredText: http://docutils.sourceforge.net/rst.html
-      long_description=long_descr,
-      author="Phil Budne",
-      author_email="phil@ultimate.com",
+      long_description="",
+      author="Ken Spencer",
+      author_email="ken@electrocode.net",
       url=url,
       download_url=download_url,
       py_modules=['follow'],
       classifiers=[
-        # http://pypi.python.org/pypi?:action=list_classifiers
         'Development Status :: 3 - Alpha',
         #"Development Status :: 4 - Beta",
         #"Development Status :: 5 - Production/Stable",
@@ -37,6 +22,6 @@ setup(name=name,
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      license="MIT",
+      license="GPL 3",
       platforms = ['Any']
       )
